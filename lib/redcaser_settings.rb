25225f1f49a0fd460bb1b1cdbdc48ee190a74d9f
Codruț Constantin Gușoi <codrut.gusoi@zitec.com>
2016-02-15 19:24:43 +0200

    Accessing tracker and status names in javascript

--- lib/redcaser_settings.rb
+++ lib/redcaser_settings.rb
@@ -4,9 +4,19 @@ class RedcaserSettings
     setting.blank? ? nil : setting.to_i
   end
 
+  def self.tracker_name
+    tracker = Tracker.where(id: tracker_id).first
+    tracker ? tracker.name : nil
+  end
+
   def self.status_new_id
     setting = settings['status_new']
     setting.blank? ? nil : setting.to_i
+  end
+
+  def self.status_new_name
+    status = IssueStatus.where(id: status_new_id).first
+    status ? status.name : nil
   end
 
   def self.status_active_id
@@ -14,9 +24,19 @@ class RedcaserSettings
     setting.blank? ? nil : setting.to_i
   end
 
+  def self.status_active_name
+    status = IssueStatus.where(id: status_active_id).first
+    status ? status.name : nil
+  end
+
   def self.status_obsolete_id
     setting = settings['status_obsolete']
     setting.blank? ? nil : setting.to_i
+  end
+
+  def self.status_obsolete_name
+    status = IssueStatus.where(id: status_obsolete_id).first
+    status ? status.name : nil
   end
 
   private
--- spec/lib/redcaser_settings_spec.rb
+++ spec/lib/redcaser_settings_spec.rb
@@ -10,16 +10,32 @@ describe RedcaserSettings do
       expect(RedcaserSettings.tracker_id).to be_nil
     end
 
+    it 'return nil for tracker name' do
+      expect(RedcaserSettings.tracker_name).to be_nil
+    end
+
     it 'return nil for status new id' do
       expect(RedcaserSettings.status_new_id).to be_nil
+    end
+
+    it 'return nil for status new name' do
+      expect(RedcaserSettings.status_new_name).to be_nil
     end
 
     it 'return nil for status active id' do
       expect(RedcaserSettings.status_active_id).to be_nil
     end
 
+    it 'return nil for status active name' do
+      expect(RedcaserSettings.status_active_name).to be_nil
+    end
+
     it 'return nil for status obsolete id' do
       expect(RedcaserSettings.status_obsolete_id).to be_nil
+    end
+
+    it 'return nil for status obsolete name' do
+      expect(RedcaserSettings.status_obsolete_name).to be_nil
     end
   end
 
@@ -30,16 +46,32 @@ describe RedcaserSettings do
       expect(RedcaserSettings.tracker_id).to eq(@tracker.id)
     end
 
+    it 'return nil for tracker name' do
+      expect(RedcaserSettings.tracker_name).to eq(@tracker.name)
+    end
+
     it 'return nil for status new id' do
       expect(RedcaserSettings.status_new_id).to eq(@status_new.id)
+    end
+
+    it 'return nil for status new name' do
+      expect(RedcaserSettings.status_new_name).to eq(@status_new.name)
     end
 
     it 'return nil for status active id' do
       expect(RedcaserSettings.status_active_id).to eq(@status_active.id)
     end
 
+    it 'return nil for status active name' do
+      expect(RedcaserSettings.status_active_name).to eq(@status_active.name)
+    end
+
     it 'return nil for status obsolete id' do
       expect(RedcaserSettings.status_obsolete_id).to eq(@status_obsolete.id)
+    end
+
+    it 'return nil for status obsolete name' do
+      expect(RedcaserSettings.status_obsolete_name).to eq(@status_obsolete.name)
     end
   end
 end
