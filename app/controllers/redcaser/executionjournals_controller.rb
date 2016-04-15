b96327e11e120a1c5ad0a736c483ce4d7485e7bb
Codruț Constantin Gușoi <codrut.gusoi@zitec.com>
2016-04-15 16:36:47 +0300

    Using already defined finder

--- app/controllers/redcaser/executionjournals_controller.rb
+++ app/controllers/redcaser/executionjournals_controller.rb
@@ -2,7 +2,7 @@ class Redcaser::ExecutionjournalsController < RedcaserBaseController
   def index
     journals =
       if !params[:issue_id].nil?
-        ExecutionJournal.where(issue_id: params[:issue_id])
+        ExecutionJournal.find_by_issue_id(params[:issue_id])
       else
         ExecutionJournal.order('created_on desc')
       end
