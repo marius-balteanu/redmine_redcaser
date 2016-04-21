e31609addedcd3497f2f3b2f7d113f39ea8a0a3f
Codruț Constantin Gușoi <codrut.gusoi@zitec.com>
2016-04-21 16:00:38 +0300

    Changing the js api implementation

--- assets/javascripts/api.js
+++ assets/javascripts/api.js
@@ -4,6 +4,7 @@ var RedcaserAPI = (function () {
   var self = {};
 
   self.testSuites = new function () {
+    // index :: Object
     this.index = function (params) {
       var done = params.done;
       var fail = params.fail;
@@ -18,233 +19,248 @@ var RedcaserAPI = (function () {
       request.fail(fail);
     };
 
-    this.create = function () {
+    // show :: Object
+    this.show = function (params) {
       var request = null;
     };
 
-    this.update = function () {
+    // create :: Object
+    this.create = function (params) {
       var request = null;
     };
 
-    this.destroy = function () {
+    // update :: Object
+    this.update = function (params) {
+      var request = null;
+    };
+
+    // destroy :: Object
+    this.destroy = function (params) {
+      var request = null;
+    }
+  };
+
+  self.executionSuite = new function () {
+    // index :: Object
+    this.index = function (params) {
+      var done = params.done;
+      var fail = params.fail;
+
+      var request = $.ajax({
+        dataType: 'json',
+        method:   'GET',
+        url:      'redcaser/executionsuites'
+      });
+
+      request.done(done);
+      request.fail(fail);
+    };
+
+    // show :: Object
+    this.show = function (params) {
+      var request = null;
+    };
+
+    // create :: Object
+    this.create = function (params) {
+      var request = null;
+    };
+
+    // update :: Object
+    this.update = function (params) {
+      var request = null;
+    };
+
+    // destroy :: Object
+    this.destroy = function (params) {
+      var request = null;
+    }
+  };
+
+  self.executionJournal = new function () {
+    // index :: Object
+    this.index = function (params) {
+      var done = params.done;
+      var fail = params.fail;
+
+      var request = $.ajax({
+        dataType: 'json',
+        method:   'GET',
+        url:      'redcaser/executionjournals'
+      });
+
+      request.done(done);
+      request.fail(fail);
+    };
+
+    // show :: Object
+    this.show = function (params) {
+      var request = null;
+    };
+
+    // create :: Object
+    this.create = function (params) {
+      var request = null;
+    };
+
+    // update :: Object
+    this.update = function (params) {
+      var request = null;
+    };
+
+    // destroy :: Object
+    this.destroy = function (params) {
+      var request = null;
+    }
+  };
+
+  self.environments = new function () {
+    // index :: Object
+    this.index = function (params) {
+      var done = params.done;
+      var fail = params.fail;
+
+      var request = $.ajax({
+        dataType: 'json',
+        method:   'GET',
+        url:      'redcaser/environments'
+      });
+
+      request.done(done);
+      request.fail(fail);
+    };
+
+    // show :: Object
+    this.show = function (params) {
+      var request = null;
+    };
+
+    // create :: Object
+    this.create = function (params) {
+      var request = null;
+    };
+
+    // update :: Object
+    this.update = function (params) {
+      var request = null;
+    };
+
+    // destroy :: Object
+    this.destroy = function (params) {
+      var request = null;
+    }
+  };
+
+  self.testCase = new function () {
+    // index :: Object
+    this.index = function (params) {
+      var done = params.done;
+      var fail = params.fail;
+
+      var request = $.ajax({
+        dataType: 'json',
+        method:   'GET',
+        url:      'redcaser/testcases'
+      });
+
+      request.done(done);
+      request.fail(fail);
+    };
+
+    // show :: Object
+    this.show = function (params) {
+      var request = null;
+    };
+
+    // create :: Object
+    this.create = function (params) {
+      var request = null;
+    };
+
+    // update :: Object
+    this.update = function (params) {
+      var request = null;
+    };
+
+    // destroy :: Object
+    this.destroy = function (params) {
+      var request = null;
+    }
+  };
+
+  self.combos = new function () {
+    // index :: Object
+    this.index = function (params) {
+      var done = params.done;
+      var fail = params.fail;
+
+      var request = $.ajax({
+        dataType: 'json',
+        method:   'GET',
+        url:      'redcaser/combos'
+      });
+
+      request.done(done);
+      request.fail(fail);
+    };
+
+    // show :: Object
+    this.show = function (params) {
+      var request = null;
+    };
+
+    // create :: Object
+    this.create = function (params) {
+      var request = null;
+    };
+
+    // update :: Object
+    this.update = function (params) {
+      var request = null;
+    };
+
+    // destroy :: Object
+    this.destroy = function (params) {
+      var request = null;
+    }
+  };
+
+  self.graph = new function () {
+    // index :: Object
+    this.index = function (params) {
+      var done = params.done;
+      var fail = params.fail;
+
+      var request = $.ajax({
+        dataType: 'json',
+        method:   'GET',
+        url:      'redcaser/graph'
+      });
+
+      request.done(done);
+      request.fail(fail);
+    };
+
+    // show :: Object
+    this.show = function (params) {
+      var request = null;
+    };
+
+    // create :: Object
+    this.create = function (params) {
+      var request = null;
+    };
+
+    // update :: Object
+    this.update = function (params) {
+      var request = null;
+    };
+
+    // destroy :: Object
+    this.destroy = function (params) {
       var request = null;
     }
   };
 
   return self;
 })();
-
-
-/*
-var RedcaserAPI = function () {
-  this.context = 'redcaser/';
-
-  this.core = new function () {
-    this.controller = '';
-
-    this.getAttachmentURLs = function () {
-      return {
-        method: (this.controller + '/attachment_urls'),
-        httpMethod: 'GET'
-      };
-    }.bind(this);
-  };
-
-  this.testSuite = new function () {
-    this.controller = 'testsuites';
-
-    this.index = function () {
-      return {
-        method: this.controller,
-        httpMethod: 'GET'
-      };
-    }.bind(this);
-
-    this.destroy = function (id) {
-      return {
-        method: (this.controller + '/' + id),
-        httpMethod: 'DELETE'
-      };
-    }.bind(this);
-
-    this.update = function (id) {
-      return {
-        method: (this.controller + '/' + id),
-        httpMethod: 'PUT'
-      };
-    }.bind(this);
-
-    this.create = function () {
-      return {
-        method: this.controller,
-        httpMethod: 'POST'
-      };
-    }.bind(this);
-  };
-
-  this.executionSuite = new function () {
-    this.controller = 'executionsuites';
-
-    this.create = function () {
-      return {
-        method: this.controller,
-        httpMethod: 'POST'
-      };
-    }.bind(this);
-
-    this.update = function (id) {
-      return {
-        method: (this.controller + '/' + id),
-        httpMethod: 'PUT'
-      };
-    }.bind(this);
-
-    this.destroy = function (id) {
-      return {
-        method: (this.controller + '/' + id),
-        httpMethod: 'DELETE'
-      };
-    }.bind(this);
-
-    this.show = function (id) {
-      return {
-        method: (this.controller + '/' + id),
-        httpMethod: 'GET'
-      };
-    }.bind(this);
-
-    this.index = function () {
-      return {
-        method: this.controller,
-        httpMethod: 'GET'
-      };
-    }.bind(this);
-  };
-
-  this.executionJournal = new function () {
-    this.controller = 'executionjournals';
-
-    this.index = function () {
-      return {
-        method: this.controller,
-        httpMethod: 'GET'
-      };
-    }.bind(this);
-  };
-
-  this.environments = new function () {
-    this.controller = 'environments';
-
-    this.show = function (id) {
-      return {
-        method: (this.controller + '/' + id),
-        httpMethod: 'GET'
-      };
-    }.bind(this);
-
-    this.update = function (id) {
-      return {
-        method: (this.controller + '/' + id),
-        httpMethod: 'PUT'
-      };
-    }.bind(this);
-
-    this.create = function () {
-      return {
-        method: this.controller,
-        httpMethod: 'POST'
-      };
-    }.bind(this);
-
-    this.destroy = function (id) {
-      return {
-        method: (this.controller + '/' + id),
-        httpMethod: 'DELETE'
-      };
-    }.bind(this);
-  };
-
-  this.testCase = new function () {
-    this.controller = 'testcases';
-
-    this.index = function () {
-      return {
-        method: this.controller,
-        httpMethod: 'GET'
-      };
-    }.bind(this);
-
-    this.update = function (id) {
-      return {
-        method: (this.controller + '/' + id),
-        httpMethod: 'PUT',
-      };
-    }.bind(this);
-
-    this.copy = function (id) {
-      return {
-        method: (this.controller + '/' + id + '/copy'),
-        httpMethod: 'POST',
-      };
-    }.bind(this);
-  };
-
-  this.combos = new function () {
-    this.controller = 'combos';
-
-    this.index = function () {
-      return {
-        method: this.controller,
-        httpMethod: 'GET'
-      };
-    }.bind(this);
-  };
-
-  this.graph = new function () {
-    this.controller = 'graph';
-
-    this.show = function (id) {
-      return {
-        method: (this.controller + '/' + id),
-        httpMethod: 'GET'
-      };
-    }.bind(this);
-  };
-
-  this.apiCall = function (parameters) {
-    var url = (this.context + parameters.method);
-    var token = $("meta[name='csrf-token']").attr('content');
-    var params = $.extend({},
-      parameters.params, {
-        authenticity_token: token
-      }
-    );
-    $('#ajax-indicator').fadeIn(100);
-    $.ajax(url, {
-      type: (parameters.httpMethod ? parameters.httpMethod : 'GET'),
-      data: params,
-      success: function (data, textStatus, jqXHR) {
-        if (parameters.success) {
-          parameters.success(data, textStatus, jqXHR);
-        }
-      },
-      error: function (jqXHR, textStatus, errorThrown) {
-        if (parameters.error) {
-          parameters.error(errorThrown, textStatus, jqXHR);
-        }
-        Redcaser.errorBox(parameters.errorMessage);
-      },
-      complete: function () {
-        if (parameters.complete) {
-          parameters.complete();
-        }
-        $('#ajax-indicator').fadeOut(100);
-      }
-    });
-  };
-};
-
-$(function () {
-  Redcaser.api = new RedcaserAPI();
-});
-*/
