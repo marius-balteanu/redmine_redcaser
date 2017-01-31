82a9c68a49087a859372b4b5711bdd5dbe19624a
Marius BALTEANU <marius.balteanu@zitec.com>
2017-01-31 19:42:28 +0000

    progress on rendering test case execution on server

--- app/controllers/redcaser/testcases_controller.rb
+++ app/controllers/redcaser/testcases_controller.rb
@@ -13,44 +13,12 @@ class Redcaser::TestcasesController < RedcaserBaseController
   before_action :find_test_case, only: [:show, :update, :destroy]
 
   def show
-    test_case = @test_case.to_json
-    relations = @test_case.issue.relations.select {|r| r.other_issue(@test_case.issue) && r.other_issue(@test_case.issue).visible? }
-    # related_issues =  []
-
-    # relations.each do |relation|
-    #   other_issue = relation.other_issue(@test_case.issue)
-    #   related_issues << relation.to_s(other_issue) {|other| link_to_issue(other, :project => Setting.cross_project_issue_relations?)}.html_safe + " - " + other_issue.status.to_s
-    #   # @ToDo: select only some keys (id, subject, status, assignee)
-    # end
-
-    status = TestCaseStatus.includes(:execution_result)
-      .find_by(test_case_id: @test_case.id, execution_suite: params[:execution_suite_id])
-
-    if status
-      name = status.execution_result_id ? status.execution_result.name : ''
-      test_case['status'] = {id: status.execution_result_id, test_case_status_id: status.id, name: name}
-    end
-
-    journals = @test_case.journals(params[:execution_suite_id])
-      # .map do |journal|
-      #   {
-      #     avatar:  gravatar(journal.executor.mail, :size => "24"),
-      #     author:  authoring(journal.created_on, journal.executor, :label => :label_updated_time_by),
-      #     journal: journal
-      #   }
-      # end
-
-    # respond_to do |format|
-    #   # format.html { render :action => 'new', :layout => !request.xhr? }
-    #   format.js { render: action =>}
-    # end
+    @relations = @test_case.issue.relations.select {|r| r.other_issue(@test_case.issue) && r.other_issue(@test_case.issue).visible? }
+    @journals = @test_case.journals(params[:execution_suite_id]).preload(:executor => :email_address).preload(:result)
+    @test_case_statuses = ExecutionResult.all();
 
     render :partial => 'test_cases/show'
-    # render json: {
-    #   test_case: test_case,
-    #   journals:  journals,
-    #   relations: related_issues
-    # }
+
   rescue ActiveRecord::RecordNotFound
     render_404
   end
