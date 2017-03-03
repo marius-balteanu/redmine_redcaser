9131ddd62f3d03395d336ba0d4b230a84255d12b
Marius BALTEANU <marius.balteanu@zitec.com>
2017-03-03 10:14:13 +0000

    squash all migrations into a single migration file

--- db/migrate/001_redcaser_setup.rb
+++ db/migrate/001_redcaser_setup.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class RedcaserSetup < ActiveRecord::Migration
   def change
     create_table :test_suites do |column|
@@ -13,12 +11,19 @@ class RedcaserSetup < ActiveRecord::Migration
       column.string  :name, null: false, limit: 128
       column.integer :parent_id, default: nil
       column.integer :project_id
+      column.integer :execution_suites, :environment_id
+      column.integer :execution_suites, :query_id
+      column.integer :execution_suites, :version_id
     end
     add_index :execution_suites, [:name, :parent_id], unique: true
+    add_index :execution_suites, :version_id
 
     create_table :test_cases do |column|
       column.integer :test_suite_id, null: false
       column.integer :issue_id, null: false
+      column.text :test_cases, :preconditions
+      column.text :test_cases, :steps
+      column.text :test_cases, :expected_results
     end
     add_index :test_cases, [:test_suite_id, :issue_id], unique: true,
       name: 'test_suite_test_case_index'
@@ -39,17 +44,26 @@ class RedcaserSetup < ActiveRecord::Migration
     create_table :execution_journals do |column|
       column.integer  :test_case_id, null: false
       column.integer  :result_id, null: false
-      column.integer  :version_id
+      column.integer  :execution_suite_id
       column.integer  :executor_id, null: true
-      column.integer  :environment_id
       column.text     :comment
       column.datetime :created_on, null: false
     end
+    add_index :execution_journals, :execution_suite_id
 
     create_table :execution_environments do |column|
       column.string  :name, null: false, limit: 128
       column.string  :description
       column.integer :project_id, null: false
     end
+
+    create_table :test_case_statuses do |column|
+      column.integer :execution_result_id
+      column.integer :execution_suite_id
+      column.integer :test_case_id
+    end
+    add_index :test_case_statuses, :execution_suite_id
+    add_index :test_case_statuses, :test_case_id
+
   end
 end
