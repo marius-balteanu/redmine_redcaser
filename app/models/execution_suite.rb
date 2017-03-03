a022a57ff362ea82af8cb12252081621ad2a07fc
Marius BALTEANU <marius.balteanu@zitec.com>
2017-03-03 11:07:20 +0000

    remove left code

--- app/models/execution_suite.rb
+++ app/models/execution_suite.rb
@@ -8,7 +8,4 @@ class ExecutionSuite < ActiveRecord::Base
 
   has_many :execution_suite_test_cases, class_name: "ExecutionSuiteTestCase", dependent: :delete_all
   has_many :test_cases, through: :execution_suite_test_cases
-
-  validates_uniqueness_of :execution_suite, scope: [:target_version]
-
 end
